07060a868458baf3b74baa9f34c46dc7b5c8fda9
anargarib23 <63562127+anargarib23@users.noreply.github.com>
2020-11-11 00:43:01 +0400

    Update parsers.py
    Parsers for following sites created:
    * Azadliq Radiosu
    * Meydan TV - failure (Right HTML file is not retrieved)
    * Report Az - failure (403 HTTP Error)
    * QafqazInfo - failure (403 HTTP Error)

--- parsers.py
+++ parsers.py
@@ -13,6 +13,7 @@ from urllib.request import urlopen
 from News import News
 from Site import Site
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Parsing algorithm for 'BBC Azerbaijan' news
 def parseBBCAz(query):
@@ -42,7 +43,112 @@ def parseBBCAz(query):
     for r in site.results:
         print(r.source + '|' + r.headline + '|' + r.date)
 
+    return site
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     
+# Parsing algorithm for 'Azadliq Radiosu' news
+# TODO: edit date format
+def parseAzadliq(query):
+    query = query.strip()
+    query = " ".join(query.split())
+    query = query.replace(' ', '+')
+
+    source = urlopen('https://www.azadliq.org/s?k=' + query + '&tab=all&pi=1&r=any&pp=50')
+    file = bs.BeautifulSoup(source, 'lxml')
+
+    source_tags = file.find_all('a', class_='img-wrap img-wrap--t-spac img-wrap--size-3 img-wrap--float img-wrap--xs', href=True)
+    headline_tags = file.find_all('h4', class_='media-block__title media-block__title--size-3')
+    date_tags = file.find_all('span', class_='date date--mb date--size-3')
+
+    site = Site('Azadliq Radiosu')
     
-                         
+    for i in range(len(source_tags)): 
+        source = source_tags[i]
+        headline = source['title']
+        date = date_tags[i]
+        site.results.append(News('https://www.azadliq.org' + source['href'], headline, date.string, '00:00'))
+
+    for r in site.results:
+        print(r.source + '|' + r.headline + '|' + r.date)
+
+    return site
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         
+# Parsing algorithm for 'Meydan TV' news
+# INCOMPLETE: Wrong HTML file is retrieved while searching
+def parseMeydanTv(query):
+    query = query.strip()
+    query = " ".join(query.split())
+    query = query.replace(' ', '+')
+
+    source = urlopen('https://www.meydan.tv/az/search/?query=' + query)
+    file = bs.BeautifulSoup(source, 'lxml')
+
+    print(file)
+    li_tags = file.find_all('li')
+    print(li_tags)
+    news_tags = list()
+    date_tags = list()
+
+    for li in li_tags:
+        divs = li.find_all('div')
+        print(divs)
+        print('----------------')
+        
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Parsing algorithm for 'Report Az' news
+# INCOMPLETE: 403 ERROR
+def parseReportAz(query):
+    query = query.strip()
+    query = " ".join(query.split())
+    query = query.replace(' ', '+')
+
+    source = urlopen('https://www.report.az/search?query=' + query)
+    file = bs.BeautifulSoup(source, 'lxml')
+
+    news_tags = file.find_all('a', class_='title', href=True)
+    date_tags = file.find_all('div', class_='news-date')
+
+    site = Site('Report.az')
+
+    for i in range(len(news_tags)): 
+        tag = news_tags[i]
+        date = date_tags[i]
+        site.results.append(News("https://report.az/" + tag['href'], tag.string, date.string, '00:00'))
+
+    # to print for debugging
+    for r in site.results:
+        print(r.source + '|' + r.headline + '|' + r.date)
+
+    return site
     
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#https://qafqazinfo.aznews/search
+
+# Parsing algorithm for 'QafqazInfo' news
+# INCOMPLETE: 403 ERROR
+def parseQafqazInfo(query):
+    query = query.strip()
+    query = " ".join(query.split())
+    query = query.replace(' ', '+')
+
+    source = urlopen('https://qafqazinfo.az/news/search?keyword=' + query)
+    file = bs.BeautifulSoup(source, 'lxml')
+
+    news_tags = file.find_all('a', class_='https://qafqazinfo.aznews/search', href=True)
+
+    site = Site('QafqazInfo')
+
+    for i in range(len(news_tags)): 
+        tag = news_tags[i]
+        site.results.append(News(tag['href'], tag.string, '00.00.0000', '00:00'))
+
+    for r in site.results:
+        print(r.source + '|' + r.headline + '|' + r.date)
+
+    return site
+    
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
